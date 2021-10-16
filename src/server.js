632a84cb2888499f9070702faabffc1b346d22f3
Roxy100 <misoamy16@gmail.com>
2021-10-16 15:14:33 +0900

    makinggetrequest

--- src/server.js
+++ src/server.js
@@ -6,6 +6,13 @@ const PORT = 4000;
 // express application을 만들 것임.
 const app = express();
 
+// GET Request 완료하기
+// 브라우저가 나의 서버에 무언가를 request(요청)한다.
+// GET / -> 웹사이트를 가져와달라는 rquest.
+const handleHome = () => console.log("Somebody is trying to go home.");
+
+app.get("/", handleHome);
+
 // 그 서버가 port 4000을 listening하고 있음.
 const handleListening = () =>
   console.log("Server listening on port http://localhost:${PORT} 🚀");
