5876b022ea8c8eb67b0d87ccf7dcb25c480c031e
Roxy100 <misoamy16@gmail.com>
2021-10-16 13:49:33 +0900

    myserverport

---
+++ src/server.js
@@ -0,0 +1,12 @@
+// "express"라는 package를 express라는 이름으로 import해온 것.
+import express from "express";
+
+// My first SERVER localhost:4000
+const PORT = 4000;
+// express application을 만들 것임.
+const app = express();
+// 그 서버가 port 4000을 listening하고 있음.
+const handleListening = () =>
+  console.log("Server listening on port http://localhost:${PORT} 🚀");
+
+app.listen(PORT, handleListening);
