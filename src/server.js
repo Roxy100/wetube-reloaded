4a9ecfcdb19670e21ccd59368127a0be69313215
Roxy100 <misoamy16@gmail.com>
2021-10-16 21:51:29 +0900

    installed morgan & using morgan

--- src/server.js
+++ src/server.js
@@ -1,25 +1,27 @@
 // "express"라는 package를 express라는 이름으로 import해온 것.
 import express from "express";
+// morgan은 GET, path, status code ... 모든 정보를 가지고 있음.
+import morgan from "morgan";
 
 // My first SERVER localhost:4000
 const PORT = 4000;
 // express application을 만들 것임.
 const app = express();
+// morgan을 middleware로 사용한 것.
+// dev는 옵션 중 하나.
+const logger = morgan("dev");
 
-// middleware(request와 response와 중간사이위치하는 software)만들기
-// middleware는 작업을 다음함수[next()]에게 넘기는 함수임.
-// middleware는 next함수 argument 하나가 추가된다는 점민 빼면, 그냥 일반적인 controller랑 같은 것.
-const logger = (req, res, next) => {
-  console.log("${req.method} ${req.url}");
-  next();
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("hello");
 };
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares.");
+const login = (req, res) => {
+  return res.send("login");
 };
-// app.use는 global middleware를 만들 수 있게 해준다. 즉, 어느 URL에도 작동하는 middleware!
-// 순서 --> middleware를 use하는 게 먼저오고, 그 다음에 URL의 get이 와야 함!!!!
-app.get("/", logger, handleHome);
+// logger함수는 middleware를 return해줌.
+app.use(logger);
+app.get("/", home);
+app.get("/login", login);
 
 // 그 서버가 port 4000을 listening하고 있음.
 const handleListening = () =>
