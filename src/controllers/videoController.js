97fd9a4c7a58e4b1a254d1b07324ff504befd409
Roxy100 <misoamy16@gmail.com>
2021-10-26 15:59:11 +0900

    Connecting watch.pug & edit

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -4,7 +4,7 @@ let videos = [
     rating: 5,
     comments: 2,
     createdAt: "2 minutes ago",
-    views: 59,
+    views: 1,
     id: 1,
   },
   {
@@ -27,11 +27,11 @@ let videos = [
 export const trending = (req, res) => {
   return res.render("home", { pageTitle: "Home", videos });
 }; // home.pug를 렌더링한다.
-export const see = (req, res) => {
+export const watch = (req, res) => {
   // const id = req.params.id;
   const { id } = req.params;
   const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching ${video.title}` });
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 }; // watch.pug를 렌더링한다.
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" }); // edit.pug를 렌더링한다.
 export const search = (req, res) => res.send("Search");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+import {
+  watch,
+  edit,
+  upload,
+  deleteVideo,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", upload);
