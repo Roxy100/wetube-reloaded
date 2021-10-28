939b6d28ba29a324cdda0719d5c3cbfde23a9a39
Roxy100 <misoamy16@gmail.com>
2021-10-28 22:01:00 +0900

    session and cookies

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@
 import express from "express";
 // morgan은 GET, path, status code ... 모든 정보를 가지고 있음.
 import morgan from "morgan";
+// session은 세션 등을 말하는 것.
+import session from "express-session";
 // 따로 독립되어있는 export한 라우터들을 모아 import한 것들.
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
@@ -21,6 +23,31 @@ app.use(logger);
 // express application이 form의 value들을 이해할 수 있도록 하고, form을 멋진 javascript object 형식으로 바꿔주는 middleware.
 app.use(express.urlencoded({ extended: true }));
 // videoRouter > videoController > postEdit에 있는 req.body와 연결되는 지점.
+
+// session middleware
+// express가 세션을 메모리에 저장하고 있다. 그러나, 서버 재시작되면, 세션을 잊어버리게 된다.
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+// 이 URL을 /add-one이라고 한다면,
+app.get("/add-one", (req, res, next) => {
+  // 어떤 정보를 Javascript object에 넣어보자. 세션 object안에 potato를 만들어보았다.
+  req.session.potato += 1;
+  return res.send(`${req.session.id}\n${req.session.potato}`);
+});
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
