6712cc4dcfea66fc3c62fcb9f6b2a2a8e190c026
Roxy100 <misoamy16@gmail.com>
2021-10-29 19:58:41 +0900

    Add comments Github Login part

--- README.md
+++ README.md
@@ -355,7 +355,7 @@ https://ko.wikipedia.org/wiki/HTTP_%EC%83%81%ED%83%9C_%EC%BD%94%EB%93%9C
 #### 소셜 로그인 만드는 법 (Github)
 
 1. https://github.com/settings/applications/new 여기로 가서 설정을 해준다.
-2. User를 깃헙으로 보낸다. --> https://github.com/login/oauth/authorize?client_id=입력값
+2. Github에 User를 보낸다. --> https://github.com/login/oauth/authorize?client_id=입력값
 3. URL에 있는 것들을 바꿈으로써 다양한 방법으로 User를 승인할 수 있다. --> https://github.com/login/oauth/authorize?client_id=입력값&allow_signup=false&scope
 
 - scope : 유저에게서 얼마나 많이 정보를 읽어내고 어떤 정보를 가져올 것에 대한 것.
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -70,6 +70,7 @@ export const postLogin = async (req, res) => {
 };
 
 // <Social Login by Github>
+// Github에 user를 보낸다.
 export const startGithubLogin = (req, res) => {
   const baseUrl = "https://github.com/login/oauth/authorize";
   const config = {
@@ -81,16 +82,18 @@ export const startGithubLogin = (req, res) => {
   const finalUrl = `${baseUrl}?${params}`;
   return res.redirect(finalUrl);
 };
-
+// User가 "예" 라고 하면,
 export const finishGithubLogin = async (req, res) => {
   const baseUrl = "https://github.com/login/oauth/access_token";
   const config = {
     client_id: process.env.GH_CLIENT,
     client_secret: process.env.GH_SECRET,
-    code: req.query.code,
+    code: req.query.code, // Github가 준 code
   };
   const params = new URLSearchParams(config).toString();
   const finalUrl = `${baseUrl}?${params}`;
+  // 그 URL로 POST request를 보낸다.
+  // Github은 User에게 access_token을 준다.
   const tokenRequest = await (
     await fetch(finalUrl, {
       method: "POST",
@@ -99,9 +102,13 @@ export const finishGithubLogin = async (req, res) => {
       },
     })
   ).json();
+  // access_token은 Github API와 상호작용 할 때 쓴다.
+  // access_token만 있다면, Github API에 어떤 method를 보내더라도 응답을 보내준다.
   if ("access_token" in tokenRequest) {
-    // access api
     const { access_token } = tokenRequest;
+    // User 프로필을 받기 위해 요청할 수 있다.
+    // 그 요청은 `${apiUrl}/user`로 갈 것이다.
+    // access_token을 보내면, User 데이터를 받을 수 있을 것이다.
     const apiUrl = "https://api.github.com";
     const userData = await (
       await fetch(`${apiUrl}/user`, {
@@ -110,6 +117,8 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
+    // email API에게도 요청도 보내야 한다.
+    // access_token을 보내면,
     const emailData = await (
       await fetch(`${apiUrl}/user/emails`, {
         headers: {
@@ -117,17 +126,23 @@ export const finishGithubLogin = async (req, res) => {
         },
       })
     ).json();
+    // email array를 줄 것이다.
+    // email array에서 primary이면서 verified된 email을 찾을 것이다.
     const emailObj = emailData.find(
       (email) => email.primary === true && email.verified === true
     );
+    // 만일 찾지 못한다면, User를 login페이지로 돌아가게 만든다.
     if (!emailObj) {
+      // set notification
       return res.redirect("/login");
     }
+    // 만일 email 조건을 찾게 된다면, DB에서 해당 email을 찾을 것인데...
     let user = await User.findOne({ email: emailObj.email });
+    // 해당 email을 찾지 못했을 경우, 그 email로 User를 만들 것이다.
     if (!user) {
-      // create account
+      // Create account (해당 email과 Github이 보낸 모든 데이터를 가지고)
       user = await User.create({
-        avataUrl: userData.avatar_url,
+        avatarUrl: userData.avatar_url,
         name: userData.name,
         username: userData.login,
         email: emailObj.email,
@@ -136,6 +151,7 @@ export const finishGithubLogin = async (req, res) => {
         location: userData.location,
       });
     }
+    // 해당 email을 찾았을 경우, User를 login 시킬 것이다.
     req.session.loggedIn = true;
     req.session.user = user;
     return res.redirect("/");
