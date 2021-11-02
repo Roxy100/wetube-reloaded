8bda595cd3d3aa3285dc47125dc349a0ed0586db
Roxy100 <misoamy16@gmail.com>
2021-11-02 12:35:48 +0900

    Cleaning Play, Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,21 +4,18 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volume = document.getElementById("volume");
 
-// 비디오를 멈추고 플레이 하는 function.
+// <Play, Pause>
 const handlePlayClick = (e) => {
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
-// 텍스트를 바꿔주는 function.
-const handlePause = () => (playBtn.innerText = "Play");
-const handlePlay = () => (playBtn.innerText = "Pause");
 
+// <Mute, Unnute>
 const handleMute = (e) => {};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
-video.addEventListener("pause", handlePause);
-video.addEventListener("play", handlePlay);
