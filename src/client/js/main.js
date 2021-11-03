051b507f44ac001da8b73fc761477a33085efd90
Roxy100 <misoamy16@gmail.com>
2021-11-03 16:58:05 +0900

    Set up Recorder webpack configuration and Creating startBtn

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,4 +1,5 @@
 // Webpack하기 전 최신의 코드들
 // styles.scss 연결
 import "../scss/styles.scss";
-console.log("main");
+// import regeneratorRuntime from "regenerator-runtime";
+import "regenerator-runtime";
