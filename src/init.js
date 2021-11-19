080c51e9f9325f2c2e06039ce318c1501271b33e
Roxy100 <misoamy16@gmail.com>
2021-11-19 11:30:19 +0900

    changed port

--- src/init.js
+++ src/init.js
@@ -10,7 +10,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = process.env.PORT || 4500;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
   console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
